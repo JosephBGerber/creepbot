d04dc899c8abebc7551ac1faf07970a456aba937
josephbgerber <josephbgerber@gmail.com>
2019-04-08 17:18:14 -0400

    Testing some values

--- snapshot/scheduler.py
+++ snapshot/scheduler.py
@@ -17,6 +17,6 @@ def gm_week():
 
 def setup_jobs():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(gm_week, "cron", day_of_week="2", hour="1", minute="17")
+    scheduler.add_job(gm_week, "cron", day_of_week="2", hour="1", minute="20")
     scheduler.start()
 
