26fc4c811f6577c11774afc7a143be676696746a
josephbgerber <josephbgerber@gmail.com>
2019-04-22 17:24:26 -0400

    Made changes so code will work(?)

--- wsgi.py
+++ wsgi.py
@@ -2,14 +2,18 @@ from snapshot import app as application
 from snapshot.scheduler import gm_week
 from apscheduler.schedulers.background import BackgroundScheduler
 
-def test_cron ():
+
+def test_cron():
     print("Work pls")
 
 
-if __name__ == '__main__':
+@application.before_first_request
+def init_scheduler():
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=gm_week, trigger="cron", week="*", day_of_week="6", hour="23", minute="0")
     scheduler.add_job(func=test_cron, trigger="cron", second="*/5")
     scheduler.start()
 
+
+if __name__ == '__main__':
     application.run(host="0.0.0.0", port="8080")
