16930f7e3d2951f926da0ef0f312c3619708df1a
josephbgerber <josephbgerber@gmail.com>
2019-04-22 16:46:00 -0400

    Removed flask-apscheduler. Changed scheduler to BackgroundScheduler.

--- snapshot/scheduler.py
+++ snapshot/scheduler.py
@@ -1,4 +1,3 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 from snapshot.database import DatabaseWrapper, get_workspaces
 from snapshot.slack import post_message
 from snapshot.command import gm_week_command
@@ -7,16 +6,10 @@ from snapshot.command import gm_week_command
 def gm_week():
     workspaces = get_workspaces()
     for workspace in workspaces:
+        print("Running gm_week for " + workspace["team_id"])
         db = DatabaseWrapper(workspace["team_id"])
 
         champion = db.get_last_weeks_winner()
         text = gm_week_command(champion)
 
         post_message(text, workspace["channel"], workspace["oauth"])
-
-
-def setup_jobs():
-    scheduler = BackgroundScheduler()
-
-    scheduler.start()
-
--- wsgi.py
+++ wsgi.py
@@ -1,29 +1,12 @@
 from snapshot import app as application
 from snapshot.scheduler import gm_week
-from flask_apscheduler import APScheduler
-
-
-class Config(object):
-    SCHEDULER_API_ENABLED = True
-
-
-scheduler = APScheduler()
-
-
-@scheduler.task("cron", week="*", day_of_week="6", hour="23", minute="0")
-def job():
-    gm_week()
-
-
-@scheduler.task("cron", second="*/10")
-def job2():
-    print("work please")
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 if __name__ == '__main__':
-    application.config.from_object(Config())
-
-    scheduler.init_app(application)
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=gm_week, trigger="cron", week="*", day_of_week="6", hour="23", minute="0")
+    scheduler.add_job(func=gm_week, trigger="cron", hour="*")
     scheduler.start()
 
     application.run(host="0.0.0.0", port="8080")
